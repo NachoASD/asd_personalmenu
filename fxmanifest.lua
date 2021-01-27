cc04c40fd989e2f0d1fbd292ff972a3f549ca57b
Bombay <72895600+XiosBombay@users.noreply.github.com>
2021-01-27 03:45:45 -0500

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -2,15 +2,15 @@
 ---By NachoASD @2020   ---
 ---NachoASD#5887       ---
 --------------------------
+fx_version 'cerulean'
+game 'gta5'
 
 author 'NachoASD'
 description 'Un script para tener monedas de donador'
 version '1.0.0'
 
-fx_version 'cerulean'
-game 'gta5'
-
 shared_script 'config.lua'
+
 client_scripts {
     "client.lua"
 }
